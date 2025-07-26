086d1e74d54ee81d866e4ecb49d213f497f5624d
Abdul-basit123456 <codesoftz50@gmail.com>
2025-07-26 15:17:19 +0500

    project setup complete

---
+++ client/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+ <div class="text-4xl text-red-500">
+  HelloWorld
+ </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+
